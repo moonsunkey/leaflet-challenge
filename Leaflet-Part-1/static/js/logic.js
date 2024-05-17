5b3d6a126b42c8c1eb873df8c06e3dd514892236
moonsunkey <keytodream@hotmail.com>
2024-05-17 13:42:16 -0700

    Added legend. Adjusted color pallete.

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,12 +1,12 @@
 
-//API end point for fetching earthquake data for past 30 days M4.5+ Earthquakes in GeoJSON format
+//API end point for fetching earthquake data for past 30 days M2.5+ Earthquakes in GeoJSON format
 
-'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson'
+'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson'
 
-//Define map object with options-Bali, Indonesia
+//Define map object with options-San Francisco
 const myMap=L.map("map",{
-    center: [-8.75, 116.37],
-    zoom: 12
+    center: [37.7166, -122.2830],
+    zoom: 7
   });
 
 // Adding the tile layer
@@ -14,47 +14,52 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-//Use d3 to get the data and add it to the map.
-let geoData='https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson'
+//Use d3 to get the data.
+let geoData='https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson'
 
-d3.json(geoData).then(data=>{ 
+//Create a function to determine color based on depth
+function getColor(depth) {
+    return depth > 90 ? '#ea2c2c' :
+           depth > 70 ? '#ea822c' :
+           depth > 50 ? '#ee9c00' :
+           depth > 30 ? '#eecc00' :
+           depth > 10 ? '#d4ee00' :
+           '#98ee00'; 
+};
+
+//Using Leaflet, create a map that plots all the earthquakes from your dataset based on their longitude and latitude.
+
+d3.json(geoData).then(data=>{
     L.geoJSON(data,{
-        onEachFeature:(feature,layer)=> {
-        layer.bindPopup(`<h4>${feature.properties.place}</h4><hr><p>${new Date(feature.properties.time)}</p>`)
-    }
+        pointToLayer:(feature,latlng)=>{
+        //Define marker options based on earthquake properties
+        let depth = feature.geometry.coordinates[2];
+        let radius = feature.properties.mag*3;
+        return L.circleMarker(latlng,{
+            radius: radius,
+            fillColor: getColor(depth),
+            color:'black',
+            weight:1,
+            opacity:1,
+            fillOpacity:0.7
+        }).bindPopup(`<h4>${feature.properties.place}</h4><hr>
+        <p>${new Date(feature.properties.time)}</p>
+        <p>Magnitude: ${feature.properties.mag}</p>
+        <p>Depth: ${depth} km</p>`);
+        }
     }).addTo(myMap);
 });
-
-
-// //Create a function of map
-
-// function createMap(earthquakedata) {
-//     //Define base layers for street and topograhic maps
-//     const street = L.titleLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//     });
-//     const topo = L.topoLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-//         attribution: '&copy; OpenTopoMap, <a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>'
-//     });
-// }
-
-// //Create earthquake layer using GeoJSON data
-// const earthquakes = L.geoJSON(earthquakedata, {
-//     onEachFeature: (feature,layer) =>layer.bindPopUp(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`)
-// });
-
-
 
 // //Define layer groups for base and overlay layers
 // const baseMaps={
 //     Street Map:street,
-//     Topographic Map:topo
+//   
 // };
 
 // const overlayMaps={
 //     Earthquakes: earthquakes
 // }
-// //Using Leaflet, create a map that plots all the earthquakes from your dataset based on their longitude and latitude.
+// 
 
 // //Your data markers should reflect the magnitude of the earthquake by their size and the depth of the earthquake by color. Earthquakes with higher magnitudes should appear larger, and earthquakes with greater depth should appear darker in color.
 // const mag = feature.properties.mag
@@ -63,4 +68,58 @@ d3.json(geoData).then(data=>{
 // const depth = d3.json(queryurl).Feature.geometry.coordinates[2]
 // //Include popups that provide additional information about the earthquake when its associated marker is clicked.
 
-// //Create a legend that will provide context for your map data.
+
+
+
+
+//Create a legend that will provide context for your map data.
+
+// let legend = L.control({ position: "bottomright" });
+// legend.onAdd = function() {
+//   let div = L.DomUtil.create("div", "info legend");
+//   let limits = [-10,10,30,50,70,90,100];
+//   let labels = [];
+
+//   // Create header for the legend
+//   div.innerHTML = '<h4>Depth (km)</h4><ul>';
+
+//   // Add color blocks with labels
+//   for (let i = 0; i < limits.length - 1; i++) {
+//       let color = getColor(limits[i]);
+//       div.innerHTML += `<li style="background-color: ${color};">${limits[i]}-${limits[i+1]}</li>`;
+//   }
+
+//   // Close the list HTML
+//   div.innerHTML += '</ul>';
+//   return div;
+// };
+
+// // // Adding the legend to the map
+// legend.addTo(myMap);
+
+let legend = L.control({
+    position: "bottomright"
+})
+
+legend.onAdd= function () {
+    let div = L.DomUtil.create("div", "info legend");
+    const title = "Earthquake Depths in km"; // Define the title text
+    let grades = [-10, 10, 30, 50, 70 ,90];
+    let colors = [
+        "#98ee00",
+        "#d4ee00",
+        "#eecc00",
+        "#ee9c00",
+        "#ea822c",
+        "#ea2c2c"
+    ];
+    // Add title to the legend
+    div.innerHTML = `<h4>${title}</h4>`;
+for (let i = 0 ; i < grades.length; i ++) {
+    div.innerHTML += " <li style = 'background: " + colors[i] +  "'></li>"
+    + grades[i] + ( grades[i +1] ? "&ndash;" + grades[i +1 ] + "<br>" : "+");
+}
+return div;
+}
+legend.addTo(myMap);
+
